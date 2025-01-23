95ec1a734e84862863b37d90adadaf9dbcd10d78
sreysus <sreyassabbani@gmail.com>
2025-01-23 10:21:59 -0500

    add benches
    - `gpu_multiply` is slower than naive rust implementation

---
+++ benches/matrix_multiplication.rs
@@ -0,0 +1,31 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use ndarray::arr2;
+
+use matrix_test::matrix::Matrix;
+
+use std::time::Duration;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mat1 = Matrix::new([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
+    let mat2 = Matrix::new([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]]);
+    let mata = arr2(&[[9.0, 8.0, 7.0], [6.0, 5.0, 4.0], [3.0, 2.0, 1.0]]);
+    let matb = arr2(&[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
+
+    let mut group = c.benchmark_group("matrix multiplication");
+    group.measurement_time(Duration::from_secs(10));
+
+    group.bench_function("gpu matrix multiply", |b| {
+        b.iter(|| mat2.gpu_multiply(&mat2))
+    });
+
+    group.bench_function("naive matrix multiply", |b| {
+        b.iter(|| (black_box(&mat1) * black_box(&mat2)))
+    });
+
+    group.bench_function("ndarray multiply", |b| b.iter(|| mata.dot(&matb)));
+
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
